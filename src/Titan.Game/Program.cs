dac5b0cbec2d485e1d6c1470ebaaf286a29b9efd
Jens Andersson <jens.andersson5@gmail.com>
2020-04-25 18:06:37 +0200

    Create window, show window and poll the message queue

--- src/Titan.Game/Program.cs
+++ src/Titan.Game/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using Titan.D3D11;
+using Titan.Windows.Window;
 
 namespace Titan.Game
 {
@@ -8,6 +9,27 @@ namespace Titan.Game
         static void Main(string[] args)
         {
             Console.WriteLine($"Hello World from DirectX 11 SDK version {SdkVersion.GetVersion()}");
+
+            var window = new WindowCreator()
+                .CreateWindow(new CreateWindowArguments
+                {
+                    Title = "My first .Net 5 window",
+                    Height = 600,
+                    Width = 800,
+                    Y = 300,
+                    X = 300
+                });
+            
+            window.ShowWindow();
+
+            while (true)
+            {
+                Message message = default;
+                if (window.GetMessage(ref message))
+                {
+                    Console.WriteLine($"Message {message.Value}");
+                }
+            }
         }
     }
 }
