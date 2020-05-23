f2524689f8fe8b8317d5cde863f2260055a570de
Jens Andersson <jens.andersson5@gmail.com>
2020-05-23 07:28:11 +0200

    Remove old/unnused code

--- src/Titan/EntityComponentSystem/Components/IComponentObjectPool.cs
+++
@@ -1,8 +0,0 @@
-namespace Titan.EntityComponentSystem.Components
-{
-    // TODO: implement this for components that uses class
-    internal interface IComponentObjectPool<T> where T : class, new()
-    {
-
-    }
-}
