108303db5db15933e02194c0a12a5fdc7d9125be
Jens Andersson <jens.andersson5@gmail.com>
2020-06-21 10:12:32 +0200

    Update build script

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 param(
-    [string]$configuration = "debug"
+    [string]$configuration = "release"
 )
 
-$vcVars = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Auxiliary\Build\vcvars64.bat"
+# $vcVars = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Auxiliary\Build\vcvars64.bat"
 
-&dotnet publish src/Titan.Game/Titan.Game.csproj -r win10-x64 --self-contained true -c $configuration -o bin/win10-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true
+&dotnet publish src/Titan.Game/Titan.Game.csproj -r win10-x64 --self-contained true -c $configuration -o bin/win10-x64  -p:PublishTrimmed=true -p:PublishSingleFile=true
